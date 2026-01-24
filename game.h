bdf80e15020a3bdc86d72732a89cf4e3bd384243
Ritu <adhikaritu806@gmail.com>
2026-01-24 08:23:51 +0545

    Add isboardfull function declaration to game.h

--- game.h
+++ game.h
@@ -7,7 +7,9 @@
 void initBoard(int board[3][3]);
 int iscellempty(int board[3][3], int row, int col);
 void makemove(int board[3][3], int row, int col, int player);
+int isboardfull(int board[3][3]);
 int checkwin(int board[3][3]);
  #endif
+
 
   
