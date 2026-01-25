eb2366177b7cd6d2650bc8df94e1d2385f9e4cdc
Ritu <adhikaritu806@gmail.com>
2026-01-25 23:35:43 +0545

    Add resetgame function prototype
    Added resetgame function prototype to game.h

--- game.h
+++ game.h
@@ -9,7 +9,9 @@ int iscellempty(int board[3][3], int row, int col);
 void makemove(int board[3][3], int row, int col, int player);
 int isboardfull(int board[3][3]);
 int checkwin(int board[3][3]);
+void resetgame(int board[3][3], int *player,int *gameover);
  #endif
 
 
   
+
